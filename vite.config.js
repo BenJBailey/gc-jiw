d1bed95eedc20689a6a28ede1b86b26f59af5367
Ben <benjaminjbailey@gmail.com>
2025-10-22 18:06:59 -0500

    feat(build): add github action

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default {
+  base: "/gc-jiw/",
   plugins: [
     basicSsl({
       /** name of certification */
@@ -14,22 +15,22 @@ export default {
   build: {
     rollupOptions: {
       output: {
-        format: 'es',
+        format: "es",
         manualChunks: (id) => {
           // Keep node_modules as separate chunks
-          if (id.includes('node_modules')) {
-            return 'vendor';
+          if (id.includes("node_modules")) {
+            return "vendor";
           }
         },
         assetFileNames: (assetInfo) => {
           // Don't hash the main CSS file
-          if (assetInfo.name.endsWith('.css')) {
-            return 'assets/[name].css';
+          if (assetInfo.name.endsWith(".css")) {
+            return "assets/[name].css";
           }
-          return 'assets/[name]-[hash][extname]';
+          return "assets/[name]-[hash][extname]";
         },
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name]-[hash].js",
       },
     },
   },
