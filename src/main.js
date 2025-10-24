e7071583707e32842c18806533dfc8b6043ab4c2
Ben <benjaminjbailey@gmail.com>
2025-10-23 20:27:21 -0500

    feat(form): add amounts

--- src/main.js
+++ src/main.js
@@ -11,6 +11,44 @@ const currencyFormatter = new Intl.NumberFormat("en-US", {
 
 const progressBox = document.getElementById("progress-completed");
 const testProgressInput = document.getElementById("test-progress");
+const givingAmountButtons = document.querySelectorAll(".giving-amounts button");
+const givingSelection = document.querySelector(".giving-selection");
+const otherAmount = document.querySelector(".other-amount");
+const submitButton = document.querySelector("#giving-form #give-button");
+const updateGivingURL = () => {
+  const amount = givingSelection.value;
+  const existingURL = new URL(submitButton.getAttribute("href"));
+  const query = new URLSearchParams(existingURL.search);
+  query.set("default_amount", amount);
+  existingURL.search = query.toString();
+  submitButton.setAttribute("href", existingURL.toString());
+};
+givingAmountButtons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    const val = e.target.value;
+    givingSelection.value = val == "other" ? otherAmount.value : val;
+    givingAmountButtons.forEach((otherButton) =>
+      otherButton.classList.remove("selected-amount")
+    );
+    button.classList.add("selected-amount");
+    if (val == "other") {
+      otherAmount.removeAttribute("disabled");
+      otherAmount.focus();
+      submitButton.innerHTML =
+        otherAmount.value != "" ? `Give $${otherAmount.value}` : "Give";
+    } else {
+      otherAmount.setAttribute("disabled", "disabled");
+      submitButton.innerText = `Give $${val}`;
+    }
+    updateGivingURL();
+  })
+);
+
+otherAmount.addEventListener("keyup", (e) => {
+  submitButton.innerText = `Give $${e.target.value}`;
+  givingSelection.value = e.target.value;
+  updateGivingURL();
+});
 
 const updateProgressOnPage = ({ goal, raised }) => {
   const trackingTotal = document.getElementById("tracking-progress");
